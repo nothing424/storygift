93a65a4292a420b02235ccddbe399e7608ec9a91
nothing424 <tzerothcoret@gmail.com>
2026-06-21 08:56:44 +0700

    Upload: src/app/explore/page.tsx

---
+++ src/app/explore/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import ExploreContent from "@/components/ExploreContent";
+
+export default function ExplorePage() {
+  return (
+    <>
+      <Navbar />
+      <ExploreContent />
+      <Footer />
+    </>
+  );
+}
