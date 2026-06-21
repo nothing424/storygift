c16c1e87ace66928932185ec265ed93760d9f004
nothing424 <tzerothcoret@gmail.com>
2026-06-21 08:56:40 +0700

    Upload: src/app/create/page.tsx

---
+++ src/app/create/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/Navbar";
+import CreateFlow from "@/components/CreateFlow";
+
+export default function CreatePage() {
+  return (
+    <>
+      <Navbar />
+      <CreateFlow />
+    </>
+  );
+}
