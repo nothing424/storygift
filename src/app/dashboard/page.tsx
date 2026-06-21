24d4a10d1479caabd136e6c7227474d2f12d3908
nothing424 <tzerothcoret@gmail.com>
2026-06-21 08:56:42 +0700

    Upload: src/app/dashboard/page.tsx

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import DashboardContent from "@/components/DashboardContent";
+
+export default function DashboardPage() {
+  return (
+    <>
+      <Navbar />
+      <DashboardContent />
+      <Footer />
+    </>
+  );
+}
